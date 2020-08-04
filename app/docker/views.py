bf9a817a8a175597126055e016f419b32c655425
hzt <1209892722@qq.com>
2020-08-04 11:35:17 +0800

    dev

--- app/docker/views.py
+++ app/docker/views.py
@@ -11,15 +11,29 @@ def contains_list():
     resp_message = {
         'code': 0,
         'contains': list(),
-        'message': 'Empty。'
+        'message': 'Empty'
     }
 
     contains = docker_client.containers.list()
     if contains:
+        resp_message['code'] = 1
         resp_message['message'] = "succeed"
         for c in contains:
             resp_message['contains'].append(
-                {'name': c.attrs['Name'], 'Status': c.attrs['State']['Status']})
+                {'name': c.attrs['Name'],
+                 'Status': c.attrs['State']['Status'],
+                 'id_': c.attrs['Id']}
+            )
 
     return jsonify(resp_message)
 
+
+@docker_control.route("/start", methods=["GET", "POST"])
+def contains_start():
+
+    resp_message = {
+        'code': 0,
+        'message': 'Parameter is invalid'
+    }
+
+    return jsonify(resp_message)
