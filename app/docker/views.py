2c9b22943fe2ea73f1581b2209a8e1cf8ce03c02
hzt <1209892722@qq.com>
2020-08-04 17:08:40 +0800

    dev

--- app/docker/views.py
+++ app/docker/views.py
@@ -34,59 +34,41 @@ def contains_list():
 @docker_control.route("/start", methods=["POST"])
 def contains_start():
     """
-    request {
-        "contains": [{"id_": "", "status": "", }]
-    }
+    request {"id": ["1e3967531655", "f252c997504d"]}
     :return:
     """
     resp_message = {
         'code': 0,
     }
+    id_list = request.json.get('id')
 
-    contains_list = request.json.get('contains')
-    try:
-        assert isinstance(contains_list, list)
-        for c in contains_list:
-            assert isinstance(c, dict)
-            container = docker_client.containers.get(c.get('id_'))
-            if container and c.get('status') != 'running':
-                container.start()
+    for i in id_list:
+        container = docker_client.containers.get(i)
+        if container.status == 'exited':
+            container.start()
+    else:
         resp_message['code'] = 1
-    except Exception as e:
-        pass
+
     return jsonify(resp_message)
 
 
 @docker_control.route("/stop", methods=["POST"])
 def contains_stop():
     """
-    request {
-        "contains": [{"id_": "", "status": "", }]
-    }
+    request {"id": ["1e3967531655", "f252c997504d"]}
     :return:
     """
     resp_message = {
         'code': 0,
     }
 
-    # contains_list = request.json.get('contains')
-    # try:
-    #     assert isinstance(contains_list, list)
-    #     for c in contains_list:
-    #         assert isinstance(c, dict)
-    #         container = docker_client.containers.get(c.get('id_'))
-    #         if container and c.get('status') == 'running':
-    #             container.stop()
-    #     resp_message['code'] = 1
-    # except Exception as e:
-    #     pass
     id_list = request.json.get('id')
-    try:
-        for i in id_list:
-            container = docker_client.containers.get(i)
-            print(container.status)
+
+    for i in id_list:
+        container = docker_client.containers.get(i)
+        if container.status == 'running':
             container.stop()
+    else:
         resp_message['code'] = 1
-    except Exception as e:
-        pass
+
     return jsonify(resp_message)
