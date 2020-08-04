d1b2195de77138fc7d7126a8273c5b2e3d860459
hzt <1209892722@qq.com>
2020-08-04 11:07:37 +0800

    dev

--- app/docker/views.py
+++ app/docker/views.py
@@ -10,14 +10,14 @@ def contains_list():
 
     resp_message = {
         'code': 0,
-        'contains': {'name': '', 'Status': ''},
+        'contains': list(),
         'message': 'Parameter is invalid'
     }
 
     contains = docker_client.containers.list()
     for c in contains:
-        resp_message['contains']['name'] = c.attrs['Name']
-        resp_message['contains']['Status'] = c.attrs['State']['Status']
+        resp_message['contains'].append(
+            {'name': c.attrs['Name'], 'Status': c.attrs['State']['Status']})
 
     return jsonify(resp_message)
 
